2140f55afcaf1e82c94afc810ace62da4607c956
Daniel <Daniel@DESKTOP-VH4FTS4>
2017-03-21 12:04:45 +0100

    cas7

--- php2017/cas7/contact.php
+++ php2017/cas7/contact.php
@@ -1,4 +1,39 @@
 <?php include 'include/header.php' ?>
+<?php 
+
+    $name = '';
+    $phone = '';
+    $email = '';
+    $message = '';
+    $errors = array();
+    
+        if(isset($_POST['submit'])){
+            $name = trim($_POST['name']);
+                if($_POST['name'] == ''){
+                    $errors['name'] = "Your name is required";
+                }
+        }
+        if(isset($_POST['submit'])){
+            $phone = trim($_POST['phone']);
+                if(is_numeric($_POST['phone'])){ 
+            }
+            else{
+                $errors['phone'] = "Phone number must be numeric";
+            }
+        }
+        if(isset($_POST['submit'])){
+            $email = trim($_POST['email']);
+                if($_POST['email'] == ''){
+                    $errors['email'] = "Your email is required";
+                }
+        }
+        if(isset($_POST['submit'])){
+            $message = trim($_POST['message']);
+                if($_POST['message'] == ''){
+                    $errors['message'] = "Write your message";
+                }
+        }
+?>
 
     <!-- Page Content -->
     <div class="container">
@@ -19,35 +54,38 @@
         <div class="row">
             <div class="col-md-8">
                 <h3>Send us a Message</h3>
-                <form name="sentMessage" id="contactForm" novalidate>
+                <form method="POST" name="sentMessage" id="contactForm" novalidate>
                     <div class="control-group form-group">
                         <div class="controls">
                             <label>Full Name:</label>
-                            <input type="text" class="form-control" id="name" required data-validation-required-message="Please enter your name.">
-                            <p class="help-block"></p>
+                            <input value="<?= $name; ?>" type="text" class="form-control" id="name" name="name" required data-validation-required-message="Please enter your name.">
+                            <p class="help-block"><?php echo isset($errors['name']) ? $errors['name'] : $erros = ''; ?></p>
                         </div>
                     </div>
                     <div class="control-group form-group">
                         <div class="controls">
                             <label>Phone Number:</label>
-                            <input type="tel" class="form-control" id="phone" required data-validation-required-message="Please enter your phone number.">
+                            <input value="<?= $phone; ?>" type="tel" class="form-control" id="phone" name="phone" required data-validation-required-message="Please enter your phone number.">
+                            <p class="help-block"><?php echo isset($errors['phone']) ? $errors['phone'] : $erros = ''; ?></p>
                         </div>
                     </div>
                     <div class="control-group form-group">
                         <div class="controls">
                             <label>Email Address:</label>
-                            <input type="email" class="form-control" id="email" required data-validation-required-message="Please enter your email address.">
+                            <input value="<?= $email; ?>" type="email" class="form-control" id="email" name="email" required data-validation-required-message="Please enter your email address.">
+                            <p class="help-block"><?php echo isset($errors['email']) ? $errors['email'] : $erros = ''; ?></p>
                         </div>
                     </div>
                     <div class="control-group form-group">
                         <div class="controls">
                             <label>Message:</label>
-                            <textarea rows="10" cols="100" class="form-control" id="message" required data-validation-required-message="Please enter your message" maxlength="999" style="resize:none"></textarea>
+                            <textarea value="<?= $email; ?>" rows="10" cols="100" class="form-control" id="message" name="message" required data-validation-required-message="Please enter your message" maxlength="999" style="resize:none"></textarea>
+                            <p class="help-block"><?php echo isset($errors['message']) ? $errors['message'] : $erros = ''; ?></p>
                         </div>
                     </div>
                     <div id="success"></div>
                     <!-- For success/fail messages -->
-                    <button type="submit" class="btn btn-primary">Send Message</button>
+                    <button type="submit" class="btn btn-primary" name="submit">Send Message</button>
                 </form>
             </div>
 
