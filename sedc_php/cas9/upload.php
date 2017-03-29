582a101dd9cb456ce04c63b1edd51ca71b4da273
e-talevska <e.talevska@hotmail.com>
2017-03-29 20:48:03 +0200

    Working with file upload

--- sedc_php/cas9/upload.php
+++ sedc_php/cas9/upload.php
@@ -1,12 +1,43 @@
+<?php
+session_start();
+?>
 <html>
     <head>
         <title>File Upload</title>
     </head>
     <body>
+        <?php 
+        if(isset($_SESSION['upload_error'])) {
+            echo "<p style='color:red;'>{$_SESSION['upload_error']}</p>";
+            unset($_SESSION['upload_error']);
+        } else if(isset ($_SESSION['upload_success'])) {
+            echo "<p style='color:green;'>".$_SESSION['upload_success']."</p>";
+            unset($_SESSION['upload_success']);
+        }
+        ?>
         <form action="process_upload.php" 
               method="POST" enctype="multipart/form-data">
             <input type="file" name="uploaded_file" >
             <button name="upload">Upload</button>
         </form>
+        <ul>
+        <?php 
+        $extensions = ['png', 'jpeg', 'jpg', 'gif'];
+        $files = scandir('uploads');
+            foreach ($files as $file) {
+                if($file != '.' && $file != '..') {
+                    $pathinfo = pathinfo($file);
+                    if(in_array($pathinfo['extension'], $extensions)) {
+                        echo "<li>"
+                                . "<img src='uploads/$file' >"
+                                . "<a href='delete_file.php?file=$file'>[x]</a>"
+                            . "</li>";
+                    } else {
+                        echo "<li>$file <a href='delete_file.php?file=$file'>[x]</a></li>";
+                    }
+                }
+            }
+        ?>
+        </ul>
     </body>
 </html>
