291563bc5c779364f09bb146336f2f2242c3001a
code <ivica.trajcev@hotmail.com>
2017-03-29 20:48:29 +0200

    working with file upload

---
+++ phpcourse2017/cas10/studenti.php
@@ -0,0 +1,12 @@
+<?php
+    $student1 = "Petko Petkovski";
+    $student2 = "Marko Markovski";
+    $predmeti1 = ["prv" => "Matematika", "vtor" => "Istorija"];
+    $predmeti2 = ["tret" =>'Informatika', "cetvrti" => 'Fizika'];
+        $studentEden = "Studentot $student1 gi uci slednive predmeti: $predmeti1[prv], $predmeti1[vtor]";
+        echo $studentEden;
+        echo "<br>";
+        $studentDva = "Studentot $student2 gi uci slednive predmeti: $predmeti2[tret], $predmeti2[cetvrti]";
+        echo $studentDva;
+?>
+
