c23945f7112e9f6c8614b578e59509f323848e14
n-loshkovski <code@ANI02.SEDC.local>
2017-03-29 20:47:19 +0200

    working with file/ file upload

--- php2017/class09/process_upload.php
+++ php2017/class09/process_upload.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
  $errors = [
      UPLOAD_ERR_INI_SIZE => 'Larger than max upload size',
      UPLOAD_ERR_FORM_SIZE => 'Larger than max post size',
@@ -8,9 +9,14 @@
      UPLOAD_ERR_CANT_WRITE => 'Upload folder permissions error',
      UPLOAD_ERR_EXTENSION => 'Extension not supported'
  ];
+
  if(isset($_POST['upload'])) {
      if(isset($_FILES['uploaded_file']) && $_FILES['uploaded_file']['error'] == 0) {
              move_uploaded_file($_FILES['uploaded_file']['tmp_name'], 'uploads/'. 
                      $_FILES['uploaded_file']['name']);
+             $_SESSION['upload_success'] = "File uploded successfully";
+           } else if ($_FILES['uploaded_file']['error'] != 0)  {
+               $_SESSION['upload_error'] = $errors[$_FILES['uploaded_file']['error']];
            }
+           header("Location: upload.php");
  }
