c3879bcf063824556375b647baf8f9257de6c11b
arena-top100 <arena-top100@arenatop100-Aspire-E5-573>
2017-04-01 21:15:37 +0200

    CV Application fixes

--- caj/cv-application/cv_action.php
+++ caj/cv-application/cv_action.php
@@ -1,6 +1,19 @@
 <?php
     session_start();
     $errors = array();
+    $extensions = array("txt", "doc", "docx", "pdf");
+    $picture_extension = array("jpg", "jpeg", "png", "gif", "bmp");
+    
+    $file_errors = array(
+        UPLOAD_ERR_INI_SIZE => "File is larger than maximum upload size.",
+        UPLOAD_ERR_FORM_SIZE => "File is larger than max post size.",
+        UPLOAD_ERR_PARTIAL => "File partialy uploaded",
+        UPLOAD_ERR_NO_FILE => "No files selected",
+        UPLOAD_ERR_NO_TMP_DIR => "No tmp dir",
+        UPLOAD_ERR_CANT_WRITE => "Upload folder permission errors",
+        UPLOAD_ERR_EXTENSION => "Extension not supported",
+    );
+    
     if(isset($_POST['submit'])){
         if(!empty($_POST['name']) && !empty($_POST['email'])){
             $email = trim($_POST['email']);
@@ -10,14 +23,27 @@
                     var_dump($_FILES);
                     if(isset($_FILES['picture']) && $_FILES['picture']['error'] == 0){
                         if(isset($_FILES['cv']) && $_FILES['cv']['error'] == 0){
-                            
+                            $picture = pathinfo($_FILES['picture']['name']);
+                            $cv = pathinfo($_FILES['cv']['name']);
+                            if(in_array($picture['extension'], $picture_extension)){
+                                if(in_array($cv['extension'], $extensions)){
+                                    // MOVE FILES AND COUNT FILES
+                                }
+                                else{
+                                    $errors[] = "Please provide valid CV";
+                                }
+                            }
+                            else{
+                                $errors[] = "Please provide valid picture";
+                            }
                         }
                         else{
                             $errors[] = "Your CV is required.";
                         }
                     }
-                    else{
-                        $errors[] = "Picture is required, please re-upload your picture.";
+                    elseif($_FILES['picture']['error'] != 0 || $_FILES['cv']['error'] != 0){
+                        $_SESSION['errors'] = $file_errors[$_FILES['picture']['error']];
+                        $_SESSION['errors'] = $file_errors[$_FILES['cv']['error']];
                     }
                     //Problem with preg_match
 //                    if(preg_match('/^[a-zA-Z0-9]$/', $name)){
@@ -39,6 +65,13 @@
         }
     }
     $_SESSION['errors'] = $errors;
+    
+//    $file = pathinfo($_FILES['picture']['name']);
+//    echo "<pre>";
+//    var_dump($file);
+//    var_dump($_FILES['cv']);
+//    var_dump($_FILES['picture']);
+//    echo "</pre>";
     
     header("location: index.php");
 ?>
