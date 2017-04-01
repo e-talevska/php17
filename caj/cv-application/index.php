7ff3d7d323e3c0b8f5a73870e571ca5ab4014aa5
arena-top100 <arena-top100@arenatop100-Aspire-E5-573>
2017-04-01 02:27:03 +0200

    Small fix

--- caj/cv-application/index.php
+++ caj/cv-application/index.php
@@ -3,6 +3,13 @@
         <title>CV Application</title>
     </head>
     <body>
-        <<form action="" method=""></form>
+        <form action="" method="">
+            <label for="name">Name: </label>
+            <input type="text" id="name" name="name">
+            <br>
+            <label for="email">E-mail: </label>
+            <input type="e-mail" name="email" id="email">
+            <br>
+        </form>
     </body>
 </html>
