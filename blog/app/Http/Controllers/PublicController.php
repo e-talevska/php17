be28930bcf173c68da9ee89c680bd31a3682832f
kostovkire <kire.kostov@ymail.com>
2017-04-24 21:00:02 +0200

    mysql

---
+++ blog/app/Http/Controllers/PublicController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PublicController extends Controller
+{
+    public function about(){
+        return view('about', [
+        'name'=>'SEDC',
+        'team'=> [
+                'John Doe',
+                'Jane Doe',
+                'John Smith'
+                ]
+                
+                
+                
+                ]);
+    }
+}
