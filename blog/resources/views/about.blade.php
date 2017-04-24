6a7f793a04310803a371bbc7427a16d2aff7cfe9
n-loshkovski <code@ANI02.SEDC.local>
2017-04-24 20:59:56 +0200

    working with file/ file upload

---
+++ blog/resources/views/about.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts/layout')
+@section('title')
+About
+@endsection
+@section('content')
+     <h2>Hello {{ $name }}!</h2>
+        @if(!empty(@team))
+        <h3>My Team:</h3>
+        <ul>
+            @foreach($team as $member)
+               <li>{{ $member }}</li>
+            @endforeach
+        </ul>
+        @endif
+@endsection
