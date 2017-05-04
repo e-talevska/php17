1956666a7154389d3e1f420164e3f5ff4eead4c9
e-talevska <e.talevska@hotmail.com>
2017-05-04 21:09:00 +0200

    Laravel articles create/edit

--- blog/app/Http/Controllers/ArticleController.php
+++ blog/app/Http/Controllers/ArticleController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 use App\Article;
 
 class ArticleController extends Controller
 {
+    function __construct() {
+        $this->middleware(['auth','checkIfAuthor']);
+    }
+    
     public function index() {
         $articles = Article::all();
 //        var_dump(compact("articles"));
@@ -25,11 +30,72 @@ class ArticleController extends Controller
         return view('articles.create', ['categories' => $categories]);
     }
     
-    public function store(\Request $request) {
-//        var_dump($request::all());exit;
+    public function store(Request $request) {
+//        var_dump($request->all());exit;
 //        var_dump(\Auth::guest());
-        $input = $request::all();
+        $this->validate($request, [
+            'title' => 'required|min:3',
+            'body' => 'required',
+            'excerpt' => 'required',
+            'slug' => 'required|unique:articles',
+            'feature_image' => 'required|image',
+            'category_id' => 'required|integer',
+            'published_at' => 'required|date'
+        ]);
+        
+        $image = Input::file('feature_image');
+        $imageName = time().$image->getClientOriginalName();
+        $image->move('uploads', $imageName);
+        
+        $input = $request->all();
+        $input['feature_image'] = $imageName;
         $input['user_id'] = \Auth::user()->id;
         Article::create($input);
+        return redirect('articles');
+    }
+    
+    function edit($id) {
+        $article = Article::findOrFail($id);
+        $categories = \App\Category::pluck('title', 'id');
+        
+        return view('articles.edit', [
+            'article' => $article, 
+            'categories' => $categories
+        ]);
+    }
+    
+    function update($id, Request $request) {
+        $this->validate($request, [
+            'title' => 'required|min:3',
+            'body' => 'required',
+            'excerpt' => 'required',
+            'slug' => 'required|unique:articles,slug,'.$id,
+            'feature_image' => 'image',
+            'category_id' => 'required|integer',
+            'published_at' => 'required|date'
+        ]);
+        $article = Article::findOrFail($id);
+        $image = Input::file('feature_image');
+
+        $input = $request->all();
+        //new image chosen
+        if(isset($image)) {
+            $imageName = time().$image->getClientOriginalName();
+            $input['feature_image'] = $imageName;
+            $image->move('uploads', $imageName);
+            
+            //delete the old image
+            if(file_exists('uploads/'.$article->feature_image)) {
+                unlink('uploads/'.$article->feature_image);
+            }
+        }
+        
+        $article->update($input);
+        return redirect('articles/'.$article->slug);
+    }
+    
+    public function view($slug) {
+        $article = Article::all()->where('slug', $slug)->first();
+        return view('articles.view', ['article' => $article]);
     }
 }
